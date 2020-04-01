708724b25efe50b896c412bb35459f6078016193
clareccoyne <60409816+clareccoyne@users.noreply.github.com>
2020-04-01 09:22:41 -0400

    done

--- java.js
+++ java.js
@@ -7,15 +7,20 @@ let theText = document.querySelector('h2');
 let theBody = document.querySelector('body');
 let theLink = document.querySelector('a');
 let theImage = document.querySelector('img');
+let colors = ['red', 'green', 'blue', 'purple', 'yellow', 'pink', 'orange', 'white', 'black'];
+
 // console.log(theText)
 
+theBody.style.backgroundColor = 'yellow';
 theButton.addEventListener('click', isClicked);
+
 
 function isClicked() {
     console.log("clicked");
+    let colorIndex = parseInt(Math.random() * colors.length);
+    theBody.style.backgroundColor = colors[colorIndex];
     theText.style.color = "blue";
     theText.textContent = "you turned me blue!";
-    theBody.style.backgroundColor = "yellow";
     theBody.style.backgroundImage = "url('butterfly.gif')";
     theLink.style.opacity = 100
     theImage.style.opacity = 100
